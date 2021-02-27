a429f1cd92b977ff18716c2170af551112cf4319
Steve Bauman <steven_bauman@outlook.com>
2021-02-26 19:39:19 -0500

    Add twitter meta

--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -132,12 +132,12 @@ export default {
         {
           key: "og:image",
           name: "og:image",
-          content: process.env.SITE_URL + "/logo.jpg",
+          content: process.env.SITE_URL + "/logo-card.png",
         },
         {
           key: "twitter:image",
           name: "twitter:image",
-          content: process.env.SITE_URL + "/logo.jpg",
+          content: process.env.SITE_URL + "/logo-card.png",
         },
       ],
     };
