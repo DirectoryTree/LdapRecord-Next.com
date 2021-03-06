06dfd4164df69d1dd8c3c2e03af3dc57922b6614
Steve Bauman <steven_bauman@outlook.com>
2021-03-05 23:32:47 -0500

    Add directory tree logo to footer

---
+++ src/components/DirectoryTreeLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <DirectoryTreeSvg />
+</template>
+
+<script>
+import DirectoryTreeSvg from "@/assets/directory-tree-logo.svg";
+
+export default {
+  components: {
+    DirectoryTreeSvg,
+  },
+};
+</script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
-  <footer>
-    <p class="m-0 py-8 text-center text-base text-gray-400">
-      &copy; {{ new Date().getFullYear() }} DirectoryTree
-    </p>
+  <footer
+    class="d-flex flex-col justify-center items-center m-0 py-8 text-center text-base text-gray-400"
+  >
+    <g-link to="https://github.com/directorytree">
+      <DirectoryTreeSvg class="inline mb-2" width="45" height="45" />
+    </g-link>
+
+    <div>&copy; {{ new Date().getFullYear() }} DirectoryTree</div>
   </footer>
 </template>
+
+<script>
+import DirectoryTreeSvg from "@/components/DirectoryTreeLogo";
+
+export default {
+  components: {
+    DirectoryTreeSvg,
+  },
+};
+</script>
