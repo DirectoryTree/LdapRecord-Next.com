7a314b6f508027ab87977076eaab4585bf6941ba
Steve Bauman <steven_bauman@outlook.com>
2021-03-15 09:59:40 -0400

    Add hover color to directory tree logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,11 @@
     class="d-flex flex-col justify-center items-center m-0 py-8 text-center text-base text-gray-400"
   >
     <g-link to="https://github.com/directorytree">
-      <DirectoryTreeSvg class="inline mb-2" width="45" height="45" />
+      <DirectoryTreeSvg
+        class="inline mb-2 hover:text-blue-500"
+        width="45"
+        height="45"
+      />
     </g-link>
 
     <div>&copy; {{ new Date().getFullYear() }} DirectoryTree</div>
